c95a9ab5b239fb8adaee2e68cbf0bea33c6c5db1
Sushma Uttam <sushmauttam111@hotmail.com>
2021-04-25 19:46:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('popOverState', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide', style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('600ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in'))
+    ]),
+    trigger('photoState', [
+      state('move', style({
+        transform: 'translateX(-100%)',
+      })),
+      state('enlarge', style({
+        transform: 'scale(1.5)',
+      })),
+      state('spin', style({
+        transform: 'rotateY(180deg) rotateZ(90deg)',
+      })),
+      transition('show => hide', animate('2000ms ease-out')),
+      transition('* => *', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'animations';
+  show = false;
+
+  position: string;
+
+  constructor() { }
+
+  get stateName() {
+    return this.show ? 'show' : 'hide'
+  }
+
+
+  toggle() {
+    this.show = !this.show;
+  }
+
+  changePosition(newPosition: string){
+    this.position = newPosition;
+  }
+
 }
